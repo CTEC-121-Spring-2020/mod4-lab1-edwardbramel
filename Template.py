06e657282c5c8db330303e662dbf389be20972a3
Eddy Bramel <e.lamanna-bramel@students.clark.edu>
2020-05-11 11:37:43 -0700

    made a cool shape

--- Template.py
+++ Template.py
@@ -1,7 +1,7 @@
 """
 CTEC 121
-<your name>
-<assignment/lab name>
+Edward LaManna
+Mod 4 lab 1
 <assignment/lab description
 """
 
@@ -16,7 +16,24 @@ Output: return value and description
 
 from graphics import *
 def main():
+    """
     win = GraphWin("Title", 600, 300)
+
+    p = Point(100, 200)
+    x = p.getX()
+    y = p.getY()
+    print("x and y:", x, y)
+    """
+    win = GraphWin("Bills Window", 600, 600)
+    p1 = Point(50, 50)
+    p2 = Point(100, 100)
+    r1 = Rectangle(p1, p2)
+    r1.draw(win)
+
+    r2 = Rectangle(Point(200, 200), Point(250, 250))
+    r2.draw(win)
+
+    input()
 
 
 main()
